6ea5a07ee7468ddbca92d843d8fbf1fa8b042c16
Laurent H <laurent.hirth.68@gmail.com>
2021-03-21 20:44:48 +0100

    Add horizontal menu

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Laurent Hirth
+
+TODO :
+- Utiliser un scrollspy plutot qu'un menu vertical
--- index.js
+++ index.js
@@ -1,20 +1,39 @@
 // ***************** Declarations ***************** //
-const linkToData = './data.json'
+const linkToShortcuts = './data/shortcuts.json'
+const linkToCheatsheets = './data/cheatsheets.json'
+const linkToDictionary = './data/dictionary.json'
 
 // ***************** Functions ***************** //
 function fetchData () {
-  fetch(linkToData)
+  fetch(linkToShortcuts)
     .then((response) => {
       if (response.ok) {
         return response.json()
       }
     })
-    .then((data) => displayPage(data))
+    .then((data) => displayShortcuts(data))
+
+  fetch(linkToCheatsheets)
+    .then((response) => {
+      if (response.ok) {
+        return response.json()
+      }
+    })
+    .then((data) => displayCheatsheets(data))
+
+  fetch(linkToDictionary)
+    .then((response) => {
+      if (response.ok) {
+        return response.json()
+      }
+    })
+    .then((data) => displayDictionary(data))
 }
 
-function displayPage(data) {
-    const tab = document.querySelector('#list-tab') 
-    const tabContent = document.querySelector('#nav-tabContent') 
+function displayShortcuts(data) {
+    const shortcutsSection = document.querySelector('#nav-shortcuts')
+    const tab = shortcutsSection.querySelector('#shortcutsList-tab') 
+    const tabContent = shortcutsSection.querySelector('#shortcutsVerticalNav-tabContent') 
 
     for (const [category, value] of Object.entries(data)) {
         const tabBtn = document.createElement('a')
@@ -42,6 +61,56 @@ function displayPage(data) {
 
             td1.textContent = element.shortcut
             td2.textContent = element.label
+
+            tr.append(td1)
+            tr.append(td2)
+            tbody.append(tr)
+        });
+        
+        tab.append(tabBtn)
+        tabContent.append(tabContentElement)
+        
+    }
+
+    tab.querySelector('a').classList.add('active')
+    tabContent.querySelector('div:first-child').classList.add('show', 'active')
+}
+
+function displayCheatsheets(data) {
+  
+}
+
+function displayDictionary(data) {
+    const shortcutsSection = document.querySelector('#nav-dictionary')
+    const tab = shortcutsSection.querySelector('#dictionaryList-tab') 
+    const tabContent = shortcutsSection.querySelector('#dictionaryVerticalNav-tabContent') 
+
+    for (const [category, value] of Object.entries(data)) {
+        const tabBtn = document.createElement('a')
+        const tabContentElement = document.createElement('div')
+        const table = document.createElement('table')
+        const tbody = document.createElement('tbody')
+
+        tabBtn.classList.add('list-group-item', 'list-group-item-action')
+        tabBtn.id = 'list-' + category + '-list'
+        tabBtn.setAttribute('data-bs-toggle', 'list')
+        tabBtn.href = '#list-' + category
+        tabBtn.textContent = category
+        
+        tabContentElement.classList.add('tab-pane', 'fade')
+        tabContentElement.id = 'list-' + category
+        tabContentElement.append(table)
+        
+        table.classList.add('table', 'table-striped', 'table-hover', 'table-bordered')
+        table.append(tbody)
+
+        value.forEach(element => {
+            const tr = document.createElement('tr')
+            const td1 = document.createElement('td')
+            const td2 = document.createElement('td')
+
+            td1.textContent = element.word
+            td2.textContent = element.definition
 
             tr.append(td1)
             tr.append(td2)
